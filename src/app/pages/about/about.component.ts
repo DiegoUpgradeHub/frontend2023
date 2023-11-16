dd95048609563535f62043f88ffd2ea0d20dca62
DiegoUpgradeHub <diego.perez@bootcamp-upgrade.com>
2023-11-16 22:23:24 +0100

    CSS About me page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  contactPopup: boolean = false;
+  contact: string = '';
+
+  contactInstagram(){
+    this.contact = '@hallo.diego';
+    this.contactPopup = !this.contactPopup;
+  };
+  contactWhatsapp(){
+    this.contact = '+4917662676949';
+    this.contactPopup = !this.contactPopup;
+  };
+  contactGmail(){
+    this.contact = 'contact@diegoperez.es';
+    this.contactPopup = !this.contactPopup;
+  };
+
+  copyContact(){
+    navigator.clipboard.writeText(this.contact);
+    this.contactPopup = false;
+  };
+  closePopup(){
+    this.contactPopup = false;
+  };
+
 }
--- src/app/pages/about/about.module.ts
+++ src/app/pages/about/about.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
-
+import { SharedModule } from 'src/app/core/shared.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    AboutRoutingModule
+    AboutRoutingModule,
+    SharedModule
   ]
 })
 export class AboutModule { }
